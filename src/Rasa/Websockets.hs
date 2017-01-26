c4974242cf55232d84c49cb5b76cd3896868b464
Beemo <beemo@openmailbox.org>
2017-01-26 23:07:01 +0100

    event is now dispatched when a client connects and disconnects

--- src/Rasa/Websockets.hs
+++ src/Rasa/Websockets.hs
@@ -1,16 +1,14 @@
-{-# language OverloadedStrings #-}
+{-# language OverloadedStrings, Rank2Types #-}
 
-module Rasa.Websockets where
+module Rasa.Websockets (ClientId, Message, Send (..), websockets, ClientAction(..))
+where
 
 import qualified Control.Concurrent             as Concurrent
 import qualified Control.Exception              as Exception
 import qualified Control.Monad                  as Monad
 import           Control.Monad.State (liftIO)
-import           Control.Lens (use, (.=))
-import           Data.Default
-import qualified Data.List                      as List
-import qualified Data.Maybe                     as Maybe
 import qualified Data.Text                      as T
+import qualified Data.ByteString.Lazy           as BL
 import qualified Network.HTTP.Types             as Http
 import qualified Network.Wai                    as Wai
 import qualified Network.Wai.Handler.Warp       as Warp
@@ -25,18 +23,12 @@ type ClientId = Int
 type Client   = (ClientId, WS.Connection)
 type State    = [Client]
 
-newtype StateRef = StateRef (Maybe (Concurrent.MVar State))
-instance Default StateRef where
-  def = StateRef Nothing
-instance Show StateRef where
-  show (StateRef (Just _)) = "ref"
-  show (StateRef (Nothing)) = "no ref"
-
 data Message = Message T.Text deriving (Typeable)
-data Send = Send T.Text
+data Send = Send [ClientId] BL.ByteString | SendAll BL.ByteString
+data ClientAction = NewClient ClientId | LostClient ClientId
 
 nextId :: State -> ClientId
-nextId = Maybe.maybe 0 ((+) 1) . Safe.maximumMay . List.map fst
+nextId = maybe 0 ((+) 1) . Safe.maximumMay . map fst
 
 connectClient :: WS.Connection -> Concurrent.MVar State -> IO ClientId
 connectClient conn stateRef = Concurrent.modifyMVar stateRef $ \state -> do
@@ -44,47 +36,49 @@ connectClient conn stateRef = Concurrent.modifyMVar stateRef $ \state -> do
   return ((clientId, conn) :state, clientId)
 
 withoutClient :: ClientId -> State -> State
-withoutClient clientId = List.filter ((/=) clientId . fst)
+withoutClient clientId = filter ((/=) clientId . fst)
 
 disconnectClient :: ClientId -> Concurrent.MVar State -> IO ()
 disconnectClient clientId stateRef = Concurrent.modifyMVar_ stateRef $ \state ->
   return $ withoutClient clientId state
 
---listen :: forall b a. WS.Connection -> (Message -> IO a) -> IO (b)
+listen :: WS.Connection -> Dispatcher -> IO ()
 listen conn dispatch = Monad.forever $ do
-  x <- WS.receiveData conn
+  x <- (WS.receiveData conn) :: IO (T.Text)
   dispatch $ Message x
 
---wsApp :: Concurrent.MVar State -> WS.ServerApp
+wsApp :: Concurrent.MVar State -> Dispatcher -> WS.ServerApp
 wsApp stateRef dispatch pendingConn = do
   conn <- WS.acceptRequest pendingConn
   clientId <- connectClient conn stateRef
   WS.forkPingThread conn 30
   Exception.finally
-    (listen conn dispatch)
-    (disconnectClient clientId stateRef)
+    (dispatch (NewClient clientId)  >> listen conn dispatch)
+    (dispatch (LostClient clientId) >> disconnectClient clientId stateRef)
 
-networkServer :: Action ()
-networkServer = do
+websockets :: Int -> Action ()
+websockets port = do
   stateRef <- liftIO $ Concurrent.newMVar ([] :: [Client])
-  -- ext .= (StateRef $ Just stateRef)
-  onInit $ asyncEventProvider (serverStart stateRef)
+  onInit $ asyncEventProvider (serverStart stateRef port)
 
   onEveryTrigger_ (sendMsg stateRef)
 
---sendMsg :: WS.Connection -> Message -> Action ()
-sendMsg stateRef (Send msg) = liftIO $
+sendMsg :: Concurrent.MVar State -> Send -> Action ()
+sendMsg stateRef (SendAll msg) = liftIO $
   Concurrent.readMVar stateRef
     >>= Monad.mapM_ (\(_, x) -> WS.sendTextData x msg)
+sendMsg stateRef (Send clientIds msg) = liftIO $ do
+  state <- Concurrent.readMVar stateRef
+  Monad.mapM_ (\x -> WS.sendTextData (snd x) msg) (toSendto state)
+  where
+    toSendto = filter (\x -> (fst x) `elem` clientIds)
 
---serverStart :: Concurrent.MVar State -> ((Message -> IO () ) -> IO () ) -> IO ()
-serverStart state dispatch = do
-    putStrLn "starting"
-    Warp.run 3000 $ WS.websocketsOr
+serverStart :: Concurrent.MVar State -> Int -> Dispatcher -> IO ()
+serverStart state port dispatch = do
+    Warp.run port $ WS.websocketsOr
       WS.defaultConnectionOptions
       (wsApp state dispatch)
       httpApp
-    putStrLn "server stopped"
 
 httpApp :: Wai.Application
 httpApp _ respond = respond $ Wai.responseLBS Http.status400 [] "Not a websocket request"
